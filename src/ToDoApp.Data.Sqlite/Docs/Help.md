a53802e50fcd8c77159b674c68510c1904fe2196
Vishwam <vishwamkumar@outlook.com>
2025-07-17 23:24:59 -0400

    updated

--- src/ToDoApp.Data.Sqlite/Docs/Help.md
+++ src/ToDoApp.Data.Sqlite/Docs/Help.md
@@ -1,8 +1,15 @@
 ﻿
 ## Helpful Tips
--Add ToDoApp.Data.Sqlite project into this solution
--If you do want to do a clean run, you can run the following for example
+	-Add ToDoApp.Data.Sqlite project into this solution
 
-dotnet tool update --global dotnet-ef 
-dotnet ef migrations add InitialCreate --project D:\GitHub\ToDoApp.Data.Sqlite --startup-project D:\GitHub\Vishwam.RestApi.ArchStyles
-dotnet ef database update --project D:\GitHub\ToDoApp.Data.Sqlite --startup-project D:\GitHub\ToDoApp.Api.Rest.Controllers
+	-If you do want to do a clean run, you can run the following for example
+
+	dotnet tool update --global dotnet-ef 
+
+	dotnet ef migrations add InitialCreate \
+	  --project <Your Project repo path>\ToDoApp.Data.Sqlite \
+	  --startup-project <Your Project repo path>\ToDoApp.SoapApi.CoreWcf or <API Style project name>>
+
+	dotnet ef database update \
+	  --project <Your Project repo path>\ToDoApp.Data.Sqlite \
+	  --startup-project <Your Project repo path>\ToDoApp.SoapApi.CoreWcf or <API Style project name>>
--- src/ToDoApp.Data.Sqlite/Services/ToDoService.cs
+++ src/ToDoApp.Data.Sqlite/Services/ToDoService.cs
@@ -1,35 +1,40 @@
-﻿namespace ToDoApp.Data.Sqlite.Services;
+﻿
+namespace ToDoApp.Data.Sqlite.Services;
 
 public class ToDoService(AppDbContext dbContext) : ITodoService
 {
 
     public async Task<IEnumerable<ToDo>?> GetToDosAsync()
     {
-        return await dbContext.ToDos.ToListAsync();        
+        return await dbContext.ToDos
+         .AsNoTracking()
+         .ToListAsync();
     }
 
     public async Task<ToDo?> GetToDoByIdAsync(int id)
     {
-        return await dbContext.ToDos.FindAsync(id);
+        return await dbContext.ToDos
+         .AsNoTracking()
+         .FirstOrDefaultAsync(t => t.Id == id);
     }
 
     public async Task<ToDo?> AddToDoAsync(ToDo toDo)
     {
-        dbContext.ToDos.Add(toDo);
+        await dbContext.ToDos.AddAsync(toDo);
         await dbContext.SaveChangesAsync();
         return toDo;
     }
 
     public async Task<ToDo?> UpdateToDoAsync(ToDo toDo)
     {
-       var existingtoDo = await dbContext.ToDos.FindAsync(toDo.Id);
-        if (existingtoDo!=null)
-        { 
+        var existingtoDo = await dbContext.ToDos.FindAsync(toDo.Id);
+        if (existingtoDo != null)
+        {
             dbContext.Entry(toDo).State = EntityState.Modified;
             await dbContext.SaveChangesAsync();
             return toDo;
         }
-        return null;        
+        return null;
     }
 
     public async Task<bool> DeleteToDoByIdAsync(int id)
